44d2acbc3d7b6efe76a6740597f2ed5f63378876
OllieClarke <43183262+OllieClarke@users.noreply.github.com>
2024-09-18 17:39:44 +0100

    Create Lesson 2_Custom Smoothie Order Form.py

---
+++ DABW/Lesson 2_Custom Smoothie Order Form.py
@@ -0,0 +1,50 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
+
+from snowflake.snowpark.functions import col
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 fruits:",
+    my_dataframe
+)
+
+
+if ingredients_list:
+    #show the difference between st.write() and st.text()
+    # st.write(ingredients_list)
+    # st.text(ingredients_list)
+
+    #Create a blank string to put our ingredients in
+    ingredients_string = ''
+
+    #For loop to concatenate each fruit to the string
+    for fruit in ingredients_list:
+        ingredients_string += fruit +' '
+
+    #write the selected output to app
+    # st.write(ingredients_string)
+
+    #Build a sql insert statement
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+values ('""" + ingredients_string + """')"""
+
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+
+    #if there's anything in the string, then run the insert statement and write the output 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
