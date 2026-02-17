2c361f24b225cd9df90be4431dba09aae62671e5
OLLIECLARKE <oliver.clarke@theinformationlab.co.uk>
2026-02-17 18:25:04 +0000

    Up to end of lesson 4

---
+++ DSCW/DoraChecker.sql
@@ -0,0 +1,42 @@
+use role accountadmin;
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+   SELECT 'DSCW01' as step 
+   ,( select  iff(count(*)>=5, 5, 0)
+     from (
+       select model_name
+       from SNOWFLAKE.ACCOUNT_USAGE.CORTEX_AISQL_USAGE_HISTORY
+       where function_name ilike '%AI_COMPLETE%'
+       group by model_name
+          )
+     ) as actual 
+   , 5 as expected 
+   ,'Used Different models when exploring Cortex Playground' as description
+); 
+
+-- Set your worksheet drop lists
+--This DORA Check Requires that you RUN two Statements, one right after the other
+list @camillas_db.cortex_analyst.cortex_analyst_model_stage;
+
+--the above command puts information into memory that can be accessed using result_scan(last_query_id())
+-- If you have to run this check more than once, always run the LIST command immediately prior
+select grader(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DSCW02' as step
+ ,( select IFF(count(*)>0,1,0) 
+    from table(result_scan(last_query_id())) 
+    where "name" = 'cortex_analyst_model_stage/CAMILLAS_JUNE_TOURNAMENT.yaml') as actual
+ , 1 as expected
+ ,'Semantic Model Complete' as description
+); 
+
+-- Set your worksheet drop lists
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+   SELECT 'DSCW03' as step 
+   ,( select  round(count(*)/iff(count(*)=0,1,count(*)),0) as tally
+      from snowflake.account_usage.query_history
+      where query_text like '%CREATE SNOWFLAKE.ML.FORECAST camillas_practice_goal_forecasting%'
+      and execution_status = 'SUCCESS'
+     ) as actual 
+   , 1 as expected 
+   ,'Created Forecast Model' as description
+); 
---
+++ DSCW/Lesson 3.sql
@@ -0,0 +1,62 @@
+-- setup stuff
+use role sysadmin;
+create or replace database camillas_db;
+drop schema camillas_db.public;
+create schema camillas_db.cortex_analyst;
+use schema camillas_db.cortex_analyst;
+create or replace stage cortex_analyst_model_stage
+encryption = (type = 'snowflake_full')
+directory = (enable=true
+auto_refresh = true);
+create or replace warehouse llm_wh
+WITH warehouse_size = XSMALL
+auto_suspend = 150;
+
+-- make tables
+create or replace table camillas_db.cortex_analyst.camillas_teams
+( 
+    team_id number,
+    team_name varchar(50),
+    kit_color varchar(20),
+    coach varchar(100),
+    emoji_symbol varchar(5)
+);
+
+insert into camillas_db.cortex_analyst.camillas_teams
+values
+(1,'Blue Sky Strikers','cerulean','Stormy McLeod', '💙☁️⚡️'),
+(2,'Pitch Blazing Bombers','emerald','Kelly Groen','🌱🔥💣' ),
+(3,'Solar Flashing Flares','marigold','Ravi Bahsin', '☀️🔥'),
+(4,'Terracotta Tirade','terracotta','Clay Skála', '🪴💪');
+
+
+create or replace table camillas_db.cortex_analyst.match_locations
+(
+ location_id number, 
+ location_name varchar(50)
+);
+
+insert into camillas_db.cortex_analyst.match_locations
+values 
+(1, 'Main Street Park - Pitch 1'),
+(2, 'Main Street Park - Pitch 2'),
+(3, 'Central Park - North Pitch'),
+(4, 'Central Park - South Pitch');
+
+create or replace table camillas_db.cortex_analyst.match_schedule
+( 
+    home_team_id number,
+    away_team_id number,
+    location_id number,
+    match_datetime timestamp_ntz  
+);
+
+insert into camillas_db.cortex_analyst.match_schedule
+values
+(1,2,1,'2025-06-07 08:00:00'),
+(3,4,2,'2025-06-07 08:00:00'),
+(2,3,3,'2025-06-07 12:00:00'),
+(1,4,4,'2025-06-07 12:00:00'),
+(1,3,1,'2025-06-07 16:00:00'),
+(2,4,2,'2025-06-07 16:00:00')
+;
